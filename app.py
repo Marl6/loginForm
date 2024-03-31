1aa46d6f8d7d52d3d756d189012f42d5735e301c
Maelgwapo <mcalimutan_210000000860@uic.edu.ph>
2024-03-31 22:47:29 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask;
+from flask import render_template;
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
